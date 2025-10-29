6d29d46d8f46c0cd28e471bca485b856b2d668e9
JJJeff2000 <jschrock09@gmail.com>
2025-10-29 17:24:20 -0400

    Odd Input

--- diamond/Diamond.java
+++ diamond/Diamond.java
@@ -1,8 +1,8 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Diamond {
   public static void p(String value){
-    p(value);
+    System.out.println(value);
   }
 
   public static void scan_p(String value){
@@ -10,5 +10,40 @@ public class Diamond {
   }
 
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    scan_p("Enter a positive number: ");
+    int numberInput = scanner.nextInt();
+    if (numberInput % 2 == 0){
+      p("Even Input");
+    }
+    else{
+      p("Odd Input");
+      int halfInput = numberInput / 2;
+      int rowCount = 0;
+      int starCount = 1;
+      for (int j = (numberInput - 1); j > halfInput; j--) {
+        for (int i = halfInput; i > rowCount; i--) {
+          scan_p(" ");
+        }
+        for (int i = starCount; i > 0; i--) {
+          scan_p("*");
+        }
+        p("");
+        starCount += 2;
+        rowCount++;
+      }
+      for (int j = (numberInput + 1); j > halfInput; j--) {
+        for (int i = halfInput; i < rowCount; i++) {
+          scan_p(" ");
+        }
+        for (int i = starCount; i > 0; i--) {
+          scan_p("*");
+        }
+        p("");
+        starCount -= 2;
+        rowCount++;
+      }
+    }
+    scanner.close();
   }
 }
