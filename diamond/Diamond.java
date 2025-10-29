e5357f47f6072e606dbc662ef8146c6e84f94c6c
JJJeff2000 <jschrock09@gmail.com>
2025-10-29 14:43:41 -0400

    print set up

--- diamond/Diamond.java
+++ diamond/Diamond.java
@@ -1,4 +1,14 @@
+import java.util.*;
+
 public class Diamond {
+  public static void p(String value){
+    p(value);
+  }
+
+  public static void scan_p(String value){
+    System.out.print(value);
+  }
+
   public static void main(String[] args) {
   }
 }
