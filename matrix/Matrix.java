c01cb006f650d3d6b3809dfcd0448e38e4e42861
JJJeff2000 <jschrock09@gmail.com>
2025-11-12 16:53:04 -0500

    Matrix Start

--- matrix/Matrix.java
+++ matrix/Matrix.java
@@ -1,2 +1,38 @@
+import java.util.Scanner;
+
 public class Matrix {
+  public static void p(String value){
+    System.out.print(value);
+  }
+
+  int[][] matrix;
+
+  public void buildMyMatrix() {
+    Scanner scanner = new Scanner(System.in);
+    p("Please enter the number of rows: ");
+    int rows = scanner.nextInt();
+    p("Please enter the number of columns: ");
+    int columns = scanner.nextInt();
+    matrix = new int[rows][columns];
+    for(int i = 0; i < rows; i++){
+      for(int j = 0; j < columns; j++){
+        matrix[i][j] = 0;
+      }
+    }
+  }
+
+  public void printMatrix(){
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix[i].length; j++) {
+        p(matrix[i][j] + " ");
+      }
+      System.out.println();
+    }
+  }
+
+  public void swapMatrix(){
+    int temp = matrix[0][0];
+    matrix[0][0] = matrix[matrix.length-1][matrix.length-1];
+    matrix[matrix.length-1][matrix.length-1] = temp;
+  }
 }
--- matrix/RunMatrix.java
+++ matrix/RunMatrix.java
@@ -1,6 +1,50 @@
 import java.util.Scanner;
 
 public class RunMatrix {
+  public static void p(String value){
+    System.out.print(value);
+  }
+
   public static void main(String[] args) {
+    Matrix myMatrix = new Matrix();
+    Scanner scanner = new Scanner(System.in);
+    String input;
+    boolean programActive = true;
+    p("Welcome to My Matrix. \n");
+    while (programActive) {
+      p("1. Build Matrix \n");
+      p("2. Populate Matrix \n");
+      p("3. Print Matrix \n");
+      p("4. Swap Matrix \n");
+      p("5. Quit\n");
+      p("What would you like to do?: ");
+      input = scanner.nextLine();
+      String lowercaseInput = input.toLowerCase();
+      switch (lowercaseInput) {
+        case "1", "build matrix":
+          p("Building Matrix... \n");
+          myMatrix.buildMyMatrix();
+          p("Matrix Build Successful. \n");
+          break;
+        case "2", "populate matrix":
+          p("Populating Matrix... \n");
+          break;
+        case "3", "print matrix":
+          myMatrix.printMatrix();
+          break;
+        case "4", "swap matrix":
+          p("Swapping Matrix... \n");
+          myMatrix.swapMatrix();
+          p("Matrix Swap Successful. \n");
+          break;
+        case "5", "quit":
+          p("Goodbye. \n");
+          programActive = false;
+          break;
+        default:
+          p("Invalid input.");
+          break;
+      }
+    }
   }
 }
